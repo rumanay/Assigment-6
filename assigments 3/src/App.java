d08c65847e73b9a3e89de380b3fe3530b2450e62
Rumana Yeasmin <ryeasmin@oakland.edu>
2023-03-13 20:17:09 -0400

    first commit

---
+++ assigments 3/src/App.java
@@ -0,0 +1,26 @@
+import javax.management.MBeanNotificationInfo;
+
+public class App{
+    public int age;
+    public String gender;
+
+    //
+    public void ismammal(){
+        system.out.println("This is a public method ismammal() from class animal");
+
+    }
+    public void mate(){
+        system.out.println("This is a public method mate() from class animal");
+
+    }
+    public static void main (String [] args){
+        Animal MyAnimal = new animal ();
+        MyAnimal.ismammal();
+        MyAnimal.mate();
+
+        Fish myfish = new fish();
+        zabra myzebra = new zebra();
+        
+    }
+}
+}
